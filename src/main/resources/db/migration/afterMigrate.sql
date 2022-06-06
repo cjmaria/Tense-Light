77ab4647ed762635fce6847cc947d6aeeda3869e
Chris Maria <git@cjmaria.com>
2022-06-06 16:22:36 -0400

    Changed pre-seeded users

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -5,7 +5,12 @@ ALTER TABLE users AUTO_INCREMENT = 1;
 
 INSERT INTO users(firstname, lastname, email, type, isactive)
 VALUES
-    ('Chris', 'Maria', 'cmaria@jahnelgroup.com', 1, 1),
-    ('Dan', 'Kollar', 'email@example.com', 0, 1),
-    ('Jessie', 'Lucier', 'email@example.com', 0, 1)
+    ('Chris', 'Maria', 'cmaria@jahnelgroup.com', 0, 1),
+    ('Rida', 'Rahman', 'email@example.com', 1, 1),
+    ('Will', 'Bacon', 'email@example.com', 1, 1),
+    ('Chris', 'Carbone', 'email@example.com', 1, 1),
+    ('Chris', 'Vanderloo', 'email@example.com', 1, 1),
+    ('Sean', 'Watson', 'email@example.com', 1, 1),
+    ('Megan', 'TheeStreamer', 'email@example.com', 1, 1),
+    ('Gregory', 'Siemers', 'email@example.com', 1, 1)
 ;
