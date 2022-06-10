0b09066613fda519375ac12faaeda8e931e11a71
Chris Maria <git@cjmaria.com>
2022-06-10 13:42:40 -0400

    Added unit tests for UserController

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -2,7 +2,7 @@ USE tenselight;
 
 DELETE FROM users;
 ALTER TABLE users AUTO_INCREMENT = 1;
-
+-- Note: Changes to the seeded data can break test cases, reflect changes to src/test/resources/Users.json
 INSERT INTO users(firstname, lastname, email, type, isactive)
 VALUES
     ('Chris', 'Maria', 'cmaria@jahnelgroup.com', 0, 1),
