00b21f626e5fe71e052c796a66596151b3e7224f
Chris Maria <git@cjmaria.com>
2022-06-10 13:41:31 -0400

    Refactor User to add constructor with defaults, overrided equals operator for equality-by-value

--- src/main/kotlin/com/jahnelgroup/tenselight/entity/User.kt
+++ src/main/kotlin/com/jahnelgroup/tenselight/entity/User.kt
@@ -8,31 +8,29 @@ import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
-
 @Table(name = "users")
-
-class User {
-
+class User(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-
-    @Column(name = "firstname")
-    var firstName: String = ""
-
-    @Column(name = "lastname")
-    var lastName: String = ""
-
-    @Column(name = "email")
-    var email: String = ""
-
-    @Column(name = "isactive")
-    var isActive: Boolean = true
-
-    @Column(name = "type")
-    var type: Int = 1
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
+    @Column(name = "firstname") var firstName: String = "",
+    @Column(name = "lastname") var lastName: String = "",
+    @Column(name = "email") var email: String = "",
+    @Column(name = "isactive") var isActive: Boolean = true,
+    @Column(name = "type") var type: Int = 1
+) {
     fun hasIncompleteFields(): Boolean {
         return (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty())
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return (
+            other is User &&
+            id == other.id &&
+            firstName == other.firstName &&
+            lastName == other.lastName &&
+            email == other.email &&
+            isActive == other.isActive &&
+            type == other.type
+        )
     }
 }
