e2c795490798fa30268c4c6f4d3ccf5dd7106970
Chris Maria <git@cjmaria.com>
2022-06-10 13:37:14 -0400

    Linting

--- src/main/kotlin/com/jahnelgroup/tenselight/TenseLightApplication.kt
+++ src/main/kotlin/com/jahnelgroup/tenselight/TenseLightApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class TenseLightApplication
 
 fun main(args: Array<String>) {
-  runApplication<TenseLightApplication>(*args)
+    runApplication<TenseLightApplication>(*args)
 }
